f09d8cb1e5450592aec36b4e0dcca2ded14928a6
Alexander Loechel <Alexander.Loechel@lmu.de>
2024-03-18 16:11:32 +0100

    make console script nice

--- src/edutap/google_wallet_callback_handler/main.py
+++ src/edutap/google_wallet_callback_handler/main.py
@@ -1,10 +1,14 @@
-from .callback_handler import app
-
 import uvicorn
 
 
 def main():
-    uvicorn.run(app, host="127.0.0.1", port=9000, log_level="debug")
+    uvicorn.run(
+        "edutap.google_wallet_callback_handler.callback_handler:app",
+        host="127.0.0.1",
+        port=9000,
+        log_level="debug",
+        reload=True,
+    )
 
 
 if __name__ == "__main__":
